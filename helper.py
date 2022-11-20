2a8afcd0632ba4cb18339e0cd3e3c8a5bc962a60
cassian <kahemacassian@gmail.com>
2022-11-20 17:47:08 +1030

    Split to unique frames: added new sample vid

--- helper.py
+++ helper.py
@@ -1,5 +1,8 @@
 # Helper class that tells time
+import cv2
 import time
+import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity as ssim
 
 
 class Timer:
@@ -26,3 +29,51 @@ class Timer:
         """Returns the time elapsed.
         Will still return time elapsed so far even when timer is not stopped"""
         return f"{(time.perf_counter() - self._start):.2f}" if self._end == 0 else f"{(self._end - self._start):.2f}"
+
+
+def remove_dupe_frames(frames, timer: Timer, max_sim=0.95, gray_scale=True):
+    """Remove duplicate frames among the generated frames"""
+    c_frame = frames[0]
+    non_dupes = [c_frame]  # we consider the first frame to be the first unique frame
+
+    timer.start()
+    for frame in frames[1:]:
+        # channel axis
+        axis = None if gray_scale else 2
+
+        sim = ssim(frame, c_frame, channel_axis=axis)
+        if sim < max_sim:
+            # add to unique frames list
+            non_dupes.append(frame)
+            c_frame = frame
+    timer.stop()
+    print(f"Removing duplicate frames: {timer.elapsedTime()}")
+    return non_dupes
+
+
+def save_frames_to_file(frames: list, file_name: str, timer: Timer):
+    timer.start()
+    # Adding frames to given file
+    for id, frame in enumerate(frames):
+        cv2.imwrite(f'SplitFrames/{file_name}/frame {id}.jpg', frame)
+    timer.stop()
+
+    print(f"Saving frames: Elapsed time {timer.elapsedTime()}")
+
+
+def plot_frames(frames, col_limit=3):
+    fig, axes = plt.subplots(round(len(frames)/col_limit), col_limit, figsize=(15, 10))
+    rows, cols = axes.shape
+    for row in range(rows):
+        for col in range(cols):
+            if row * cols + col + 1 > len(frames):
+                # hide axis if it's not supposed to be plotted
+                axes[row][col].set_axis_off()
+                break
+            axes[row][col].imshow(frames[row * cols + col], cmap=plt.cm.gray)
+            axes[row][col].set_title(f"Frame {row * cols + col + 1}")
+
+    # adjust padding btwn subplots
+    plt.tight_layout()
+    # show plot
+    plt.show()
--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 # credit from https://techtutorialsx.com/2021/04/29/python-opencv-splitting-video-frames/amp/
 import cv2
-from helper import Timer
+from helper import Timer, remove_dupe_frames, plot_frames
 from skimage.metrics import structural_similarity as ssim
 
+capture = cv2.VideoCapture('sampleClips/5_seconds_cntdown.mp4')
 timer = Timer()
-frame_id = 0
 images = []
+gs_frames = []
 
-capture = cv2.VideoCapture('sampleClips/video.mp4')
-
+timer.start()
 while True:
     success, frame = capture.read()
     if not success:
         break
 
-    images.append((frame_id, frame))
-    frame_id += 1
-    # TODO: future implementations for frame processing
+    images.append(frame)
+    # Frame processing
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gs_frames.append(gray_frame)
+timer.stop()
 
-# print(f"Getting frames: Elapsed time {timer.elapsedTime()}")
-# print(f"Frames per second {capture.get(cv2.CAP_PROP_FPS)}")
-# Adding frames to given file
-# for id, frame in images:
-#     cv2.imwrite(f'SplitFrames/1/frame {id}.jpg', frame)
+print(f"Splitting frames: Elapsed time {timer.elapsedTime()}")
+print(f"FPS: {capture.get(cv2.CAP_PROP_FPS)}\n"
+      f"Frame count: {capture.get(cv2.CAP_PROP_FRAME_COUNT)}")
 
 capture.release()
-# if __name__ == '__main__':
-#     pass
 
+# grayscale frames only
+unique_frames = remove_dupe_frames(gs_frames, timer)
 
-def remove_dupe_frames(frames, max_sim = 0.95):
-    """Remove duplicate frames among the generated frames"""
-
+# plot unique frames
+plot_frames(unique_frames, timer)
